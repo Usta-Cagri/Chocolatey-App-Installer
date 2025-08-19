7ae93d189b825428258a37635387023c1a9f612a
Usta-Cagri <cagriakgun616@proton.me>
2025-08-20 00:45:03 +0300

    Now chocolatey is installed automatically

--- Chocolatey-App-Installer.ps1
+++ Chocolatey-App-Installer.ps1
@@ -1,3 +1,9 @@
+#Chocolatey Check
+$choco = choco 2>null
+if (choco -match "Chocolatey") {
+	Write-Output "Chocolatey Package Manager is detected!"
+
+
 #Options:
 Write-Output "Please Choose One of the Options:"
 Write-Output "1: Install the Recommended Apps"
@@ -36,9 +42,39 @@ if ($choice -eq "1") {
     $PredefinedApps = @("vscode", "Onlyoffice", "Firefox")
     Install-RecommendedApps -AppList $PredefinedApps
 } elseif ($choice -eq "2") {
-    Update-All 
+    Update-ChocoApps 
 } elseif ($choice -eq "3") {
     Install-ChocoApp
 } else {
     Write-Output "Invalid choice. Please restart the script and choose a valid option."
+}
+}
+
+else {
+
+#Chocolatey Package Manager Installation:
+$defaultexecutionpolicy = Get-ExecutionPolicy
+Write-Output "Chocolatey Package Manager is installing..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+if ((choco) -match "Chocolatey") {
+	Write-Output "Chocolatey Package Manager is succesfully installed!"
+	Write-Output "Please re-run the script if you want it to work!"
+}
+
+else {
+	Write-Output "Chocolatey is not installed because of an error😢(exiting...)"
+	exit
+}
+
+Set-ExecutionPolicy $defaultexecutionpolicy -Scope LocalMachine
+$defaultnewexecutionpolicy = Get-ExecutionPolicy
+
+if ($defaultnewexecutionpolicy -eq $defaultexecutionpolicy) {
+	Write-Output "Your Execution Policy before the installation hasn't changed!"
+}
+
+else {
+	Write-Output "Your Execution Policy might be have changed and it can cause some security problems!"
+}
 }
--- README.md
+++ README.md
@@ -23,7 +23,7 @@ However, I’m very open to feedback, suggestions, and contributions! If you fin
 4. Go to the location of **.ps1** file and run ".\Chocolatey-App-Installer.ps1" command.
 5. Choose a number and follow the instructions.
 6. Or you can easily type: 
-```irm https://raw.githubusercontent.com/Usta-Cagri/Chocolatey-App-Installer/main/Chocolatey-App-Installer.ps1 | iex ```
+```Set-ExecutionPolicy Bypass -Scope Process -Force; irm https://raw.githubusercontent.com/Usta-Cagri/Chocolatey-App-Installer/main/Chocolatey-App-Installer.ps1 | iex ```
 on an elevated powershell window if you've installed chocolatey package manager but soon this script will automate that. (This is the recommended way because it doesn't install anything to your disk and if you have a bit of terminal knowledge you can add this to you $PROFILE folder) 
 
 ---
